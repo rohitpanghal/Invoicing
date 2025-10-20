477075e81a07915b74e1f5d8d82bad73ff8c8876
rohitsinghpang <rohitsinghpanghal10@gmail.com>
2025-10-20 21:22:13 +0530

    slash removed

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: "https://invoicing-2-n83z.onrender.com/",
+  origin: "https://invoicing-2-n83z.onrender.com",
   methods: ["GET","POST","PUT","DELETE","OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
